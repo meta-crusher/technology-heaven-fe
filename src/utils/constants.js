65ff78b1745d5984059fb77379d7221849c967de
SaurabhSingh004 <66426028+SaurabhSingh004@users.noreply.github.com>
2024-10-12 14:18:31 +0530

    commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const constants = {
     GET_ALL_PRODUCTS: "http://localhost:3001/api/v1/products",
     GET_PRODUCT: "http://localhost:3001/api/v1/products/",
-    GET_PRODUCT_BY_NAME: "http://localhost:3001/api/v1/products/name/"
-
+    GET_PRODUCT_BY_NAME: "http://localhost:3001/api/v1/products/name/",
+    GET_RELATED_PRODUCTS_BASE: "http://localhost:3001/api/v1/products/", // Base URL without the product ID
+    GET_RELATED_PRODUCTS_SUFFIX: "/get_related_products"
 }
